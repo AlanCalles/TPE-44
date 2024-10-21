4b80190ea2fcdb883390aa369bec164da4468033
nahuelserrano <150851257+nahuelserrano@users.noreply.github.com>
2024-10-20 23:20:29 -0300

    Update db_distribuidora.sql

--- db_distribuidora.sql
+++ db_distribuidora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2024 a las 22:58:58
+-- Tiempo de generación: 21-10-2024 a las 04:19:01
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,94 +18,132 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_sistemadistribucion`
+-- Base de datos: `distrubuidora`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comerciente`
+-- Estructura de tabla para la tabla `comprador`
 --
 
-CREATE TABLE `comerciente` (
-  `id_comerciante` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `comprador` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `nombre_producto` varchar(30) NOT NULL,
+  `tipoDeCompra` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `comerciente`
+-- Volcado de datos para la tabla `comprador`
 --
 
-INSERT INTO `comerciente` (`id_comerciante`, `nombre`) VALUES
-(2, 'alan'),
-(1, 'nahuel');
+INSERT INTO `comprador` (`id`, `nombre`, `apellido`, `nombre_producto`, `tipoDeCompra`) VALUES
+(53, 'nahuel thiago', 'serrano roark', 'mani', 'mayorista'),
+(54, 'tomas', 'gutierrez', 'choripan', 'minorista'),
+(55, 'nahuel thiago', 'serrano roark', 'mani', 'minorista');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `distribuidora`
+-- Estructura de tabla para la tabla `empresa`
 --
 
-CREATE TABLE `distribuidora` (
-  `id_producto` int(11) NOT NULL,
-  `id_comerciante` int(11) NOT NULL,
-  `nombre_producto` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `empresa` (
+  `id_producto` int(11) DEFAULT NULL,
+  `id_comerciante` int(11) DEFAULT NULL,
+  `id_venta` int(11) NOT NULL,
+  `categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `distribuidora`
+-- Volcado de datos para la tabla `empresa`
 --
 
-INSERT INTO `distribuidora` (`id_producto`, `id_comerciante`, `nombre_producto`) VALUES
-(2, 1, 'turron'),
-(3, 2, 'alfajor');
+INSERT INTO `empresa` (`id_producto`, `id_comerciante`, `id_venta`, `categoria`) VALUES
+(NULL, NULL, 15, 'mayorista');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `gmail` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`gmail`, `password`, `id`) VALUES
+('roark.nahuelthiago@gmail.com', '$2y$10$PWJIPuNMb26bgqvwpocpK.wL88OqChDTQhr2DhemR0VbVGwSCrWOO', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `comerciente`
+-- Indices de la tabla `comprador`
 --
-ALTER TABLE `comerciente`
-  ADD PRIMARY KEY (`id_comerciante`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
+ALTER TABLE `comprador`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `distribuidora`
+-- Indices de la tabla `empresa`
 --
-ALTER TABLE `distribuidora`
-  ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_comerciante` (`id_comerciante`),
-  ADD KEY `nombre_producto` (`nombre_producto`);
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id_venta`) USING BTREE,
+  ADD UNIQUE KEY `categoria` (`categoria`),
+  ADD KEY `id_producto` (`id_producto`) USING BTREE,
+  ADD KEY `id_comerciante` (`id_comerciante`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `gmail` (`gmail`),
+  ADD UNIQUE KEY `gmail_2` (`gmail`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `comerciente`
+-- AUTO_INCREMENT de la tabla `comprador`
 --
-ALTER TABLE `comerciente`
-  MODIFY `id_comerciante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `comprador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
--- AUTO_INCREMENT de la tabla `distribuidora`
+-- AUTO_INCREMENT de la tabla `empresa`
 --
-ALTER TABLE `distribuidora`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `empresa`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `distribuidora`
+-- Filtros para la tabla `empresa`
 --
-ALTER TABLE `distribuidora`
-  ADD CONSTRAINT `distribuidora_ibfk_1` FOREIGN KEY (`id_comerciante`) REFERENCES `comerciente` (`id_comerciante`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `empresa`
+  ADD CONSTRAINT `empresa_ibfk_2` FOREIGN KEY (`id_comerciante`) REFERENCES `comprador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
